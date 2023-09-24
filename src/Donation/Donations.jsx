26b9c44e878fe49c71d5a0e3224d2b0e87440c5a
AlgoWebBot <shimulzahan636@gmail.com>
2023-09-25 04:07:32 +0600

    show all button added

--- src/Donation/Donations.jsx
+++ src/Donation/Donations.jsx
@@ -5,6 +5,7 @@ import Donation from './Donation';
 const Donations = () => {
 
   const [donations, setDonations] = useState([]);
+  const [initial, setInitial] = useState(4);
 
   useEffect(() => {
     const allDonations = searchItemInLCS();
@@ -13,10 +14,19 @@ const Donations = () => {
   
 
   return (
-    <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5'>
-      {
-        donations.map(donation=> <Donation donation={donation} /> )
-      }
+    <div className='container mx-auto'>
+      
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        {
+          donations.slice(0, initial).map(donation => <Donation donation={donation} />)
+        }
+      </div>
+      <div className='text-center mt-5'>
+        <div className={donations.length == initial && 'hidden'}>
+          <button onClick={() => setInitial(donations.length)} className='btn btn-success'>Show all</button>
+        </div>
+      </div>
+
     </div>
   )
 }
