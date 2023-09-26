28f9d42ea7b156cd42a545376b40ca4e42be4681
AlgoWebBot <shimulzahan636@gmail.com>
2023-09-26 23:19:19 +0600

    input field required add

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = ({ searchInput, searchvalue }) => {
       <div className='flex justify-center items-center flex-col h-[400px] md:h-[600px] space-y-10'>
         <p className='text-black text-center text-2xl md:text-5xl mt-8 md:mt-28 font-black'>I Grow By Helping People In Need</p>
         <div className='flex input input-bordered w-[286px] max-w-xs justify-between items-center'>
-          <input onChange={(e) => searchInput(e.target.value)} type="text" placeholder="Search here..." className=" outline-none border-none" />
+          <input onChange={(e) => searchInput(e.target.value)} type="text" required placeholder="Search here..." className=" outline-none border-none" />
           <button onClick={searchvalue} className='bg-[#FF444A] py-3 px-5 rounded-tr-lg rounded-br-lg text-base text-white'>Search</button>
         </div>
       </div>
